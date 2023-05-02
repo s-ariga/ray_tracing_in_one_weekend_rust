c498d8959fdc0f891cc791c63d8e46c916bcf903
Seiichi Ariga <s-ariga@sanctus.jp>
2023-05-02 16:39:53 +0900

    Vec3実装

---
+++ src/vec3.rs
@@ -0,0 +1,65 @@
+/* Seiichi Ariga <seiichi.ariga@gmail.com> */
+
+/* 3次元ベクトル */
+
+use std::f64::sqrt;
+use std::ops::Pow;
+// 演算子のオーバーロード用trait
+use std::ops::{Add, Mul, Div};
+
+pub struct Vec3 {
+    v: [f64; 3],
+}
+
+impl Vec3 {
+    fn length(&self) -> f64{
+        sqrt(self.length_squared())
+    }
+
+    fn length_squared(&self) -> f64 {
+        self.v[0].powi(2)+self.v[1].powi(2) + self.v[2].powi(2)
+    }
+}
+
+/* Vec3に対して、演算子をオーバーロードしていく */
+impl Add<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, other: Vec3) -> Vec3 {
+        Vec3 { 
+            v: [
+                self.v[0] + other.v[0],
+                self.v[1] + other.v[1], 
+                self.v[2] + other.v[2]
+            ],
+        }
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, t: f64) -> Vec3 {
+        Vec3 {
+            v: [self.v[0] * t, self.v[1] * t, self.v[2] * t],
+        }
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, t: f64) -> Vec3 {
+        // ここでゼロ割チェックをしておく
+        if t == 0 {
+            panic!("Div by zero in Vec3!")
+        }
+
+        Vec3 {
+            v: [self.v[0] / t, self.v[1] / t, self.v[2] / t],
+        }
+    }
+}
+
+type Point3 = Vec3;
+type Color = Vec3;
